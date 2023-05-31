a8392d4312682a4b7114243d433334a6073e2a1c
Rino Miguel Quijote <109708044+Sagario-Quijote@users.noreply.github.com>
2023-05-31 10:31:51 +1000

    Add files via upload

--- killerButtons.js
+++ killerButtons.js
@@ -1,9 +1,30 @@
-const killers = document.querySelectorAll('.killer');
-const body = document.querySelector('body');
-const killerContainer = document.querySelector("killerContainer")
+//Const variables selected through DOM
+const killers = document.querySelectorAll(".killer");
+const body = document.querySelector("body");
+const killerContainer = document.querySelector("killerContainer");
+const overlay = document.querySelector(".overlay");
 
-      killers.forEach(killer => {
-        killer.addEventListener('click', () => {
-            killer.classList.toggle("enlargeblur");
-        });
-      });
+//A variable to determine if the overlay is working.
+let isOverlayOn = false;
+
+//The overlay click event listener that allows you to click the outside of the enlarged box to return to selection.
+overlay.addEventListener("click", () => {
+  overlay.classList.remove("active");
+  killers.forEach((killer) => {
+    if (killer.classList.contains("enlarge")) {
+      killer.classList.remove("enlarge");
+    }
+  });
+  isOverlayOn = false;
+});
+
+//The killer box event listener that checks if the overlay is on. If overlay isn't on, it can be clicked to enlarge and turns makes isOverlayOn true.
+killers.forEach((killer) => {
+  killer.addEventListener("click", () => {
+    if (!isOverlayOn) {
+      killer.classList.toggle("enlarge");
+      overlay.classList.toggle("active");
+      isOverlayOn = true;
+    }
+  });
+});
